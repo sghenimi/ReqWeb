6653ef858b37f23e2c72cd3e9777c2a256a4de96
sghenimi <soufiane.ghenimi@gmail.com>
2025-05-04 14:52:24 +0200

    wip

--- generic_container.py
+++ generic_container.py
@@ -1,3 +1,7 @@
+from math import sqrt
+import numpy as np
+
+
 def first(container):
     return container[0]
 
@@ -6,3 +10,8 @@ if __name__ == "__main__":
     list_01 = ["a", "b", "c"]
     print(list_01)
     print(first(list_01))
+
+    arr_a = np.array([sqrt(i) for i in range(5)])
+    print("--"*100)
+    for i in arr_a:
+        print(i)
