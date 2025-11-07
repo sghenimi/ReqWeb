690b235f171f4563b3a3a481c19fad398b57e5af
Soufiane Ghenimi <soufiane.ghenimi@gmail.com>
2025-11-08 00:22:04 +0100

    fix endpoints

--- connect-databases/connect_sqlite_fastapi/app.py
+++ connect-databases/connect_sqlite_fastapi/app.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
-from .book import Book, BookCreate
-from .crud_book import create_book
+from connect_sqlite_fastapi.book import BookCreate, Book
+from connect_sqlite_fastapi.crud_book import create_book, get_all_books
 
 app = FastAPI()
 
@@ -9,7 +9,13 @@ app = FastAPI()
 def read_root():
  return {"message": "Welcome !"}
 
-@app.post("/books/")
+@app.get('/books')
+def get_all_users():
+    books = get_all_books()
+
+    return {"books": books}
+
+@app.post("/books/new")
 def create_book_endpoint(book: BookCreate):
  book_id = create_book(book)
  return {"id": book_id, **book.dict()}
--- connect-databases/connect_sqlite_fastapi/crud_book.py
+++ connect-databases/connect_sqlite_fastapi/crud_book.py
@@ -8,3 +8,20 @@ def create_book(book: BookCreate):
     cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (book.title, book.author))
     connection.commit()
     connection.close()
+
+def get_all_books():
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books")
+    books = cursor.fetchall()
+    connection.close()
+    return books
+
+def get_book_by_id(book_id: int):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books WHERE id = ?", (book_id,))
+    book = cursor.fetchone()
+    connection.close()
+    return book
+
