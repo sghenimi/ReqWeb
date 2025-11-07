6f86f98a83ee4fcce8597ad455b1614abfdfaf79
Soufiane Ghenimi <soufiane.ghenimi@gmail.com>
2025-11-08 00:31:00 +0100

    refactor connection

--- connect-databases/connect_sqlite_fastapi/crud_book.py
+++ connect-databases/connect_sqlite_fastapi/crud_book.py
@@ -4,24 +4,22 @@ from .init_database import create_connection
 
 def create_book(book: BookCreate):
     connection = create_connection()
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (book.title, book.author))
+    with connection.cursor() as cursor:
+        cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (book.title, book.author))
     connection.commit()
-    connection.close()
+
 
 def get_all_books():
     connection = create_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM books")
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM books")
     books = cursor.fetchall()
-    connection.close()
     return books
+
 
 def get_book_by_id(book_id: int):
     connection = create_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM books WHERE id = ?", (book_id,))
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM books WHERE id = ?", (book_id,))
     book = cursor.fetchone()
-    connection.close()
     return book
-
